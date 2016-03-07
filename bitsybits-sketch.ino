8f46974c5c9053a9259b3f9f32e068a565ab7e86
vldmkr <vladmakaryan@gmail.com>
2016-03-07 02:29:46 +0200

    initial commit

---
+++ bitsybits-sketch.ino
@@ -0,0 +1,70 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include <BitsyBits.h>
+
+SSD1306 mDisplay(&Wire);
+ConsoleView mConsoleView(&mDisplay);
+ConsoleController mConsole(&mConsoleView);
+DPad mDPad (DPAD_UP, DPAD_CENTER, DPAD_DOWN, LOW);
+bt::SchedulerTask task([] { mConsole.print("|o_O|"); });
+bt::SchedulerTask taskConsole([] {  mConsole.execute(); });
+bt::SchedulerTask taskAddDot(taskAddDotCallback);
+bt::TaskScheduler mScheduler;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(SDA, SCL);
+  Wire.setClock(400000);
+
+  pinMode(VIBRO, OUTPUT);
+
+  mDPad.init();
+
+  mDisplay.init();
+  mDisplay.clear();
+  mDisplay.display();
+  mDisplay.flipScreenVertically();
+  mDisplay.setFontScale2x2(false);
+
+  mConsole.print(KEY_WAKE_UP);
+  mConsole.print(KEY_MATRIX_HAS);
+  mConsole.print(KEY_FOLLOW);
+  mConsole.print(KEY_WHITE_RABBIT);
+
+  mConsole.print(KEY_WAKE_UP);
+  mConsole.print(KEY_MATRIX_HAS);
+  mConsole.print(KEY_FOLLOW);
+  mConsole.print(KEY_WHITE_RABBIT);
+
+  mConsole.print(KEY_WAKE_UP);
+  mConsole.print(KEY_MATRIX_HAS);
+  mConsole.print(KEY_FOLLOW);
+  mConsole.print(KEY_WHITE_RABBIT);
+
+  mConsole.print(KEY_WAKE_UP);
+  mConsole.print(KEY_MATRIX_HAS);
+  mConsole.print(KEY_FOLLOW);
+  mConsole.print(KEY_WHITE_RABBIT);
+  mScheduler.push(&task)
+  ->push(&taskConsole)
+  ->push(&taskAddDot);
+
+  task.attach(1000, true);
+  taskConsole.attach(100, true);
+  taskAddDot.attach(100, true);
+}
+
+void loop() {
+  mScheduler.execute();
+}
+
+void taskAddDotCallback() {
+  if(mDPad.isUp()){
+    mConsoleView.decRow();
+  }
+  if(mDPad.isDown()){
+    mConsoleView.incRow();
+  }
+
+  digitalWrite(VIBRO, mDPad.isCenter());
+}
